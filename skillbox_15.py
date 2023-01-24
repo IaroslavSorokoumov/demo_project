3da51686c0b0eb6d38619e3974e47d95915f21b8
Iaroslav Sorokoumov <sorokoumov2@gmail.com>
2023-01-24 20:33:42 +0300

    initial demo

---
+++ skillbox_15.py
@@ -0,0 +1,29 @@
+
+
+##for meters in 100,90,95,87,102:
+##  if meters % 3 == 0:
+##    print(meters, 'Подходит')
+##  else:
+##    print(meters, 'Не подходит')
+
+##for i in 3,7,5,6,4:
+##  print(i ** 2, i ** 3, i ** 4) 
+
+##counter = 0
+##for i in 345, 19, 87, 1020, 421, 333, 3333:
+##  if i % 3 == 0 and 10 > i // 100 > 0:
+##    print(i, 'Счастливое число!')
+##    counter += 1
+##print('Кол-во счастливых билетов:', counter)
+
+##for i in range(11):
+##  print(i ** 2)
+
+##houres = int(input('Сколько часов: '))
+##
+##for _ in range(houres):
+##  print('Ку-Ку')
+##
+
+for i in range(21):
+  print(2 ** i)
